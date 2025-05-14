c9fa183ac35e5cf3e04067a210fe3325018909f8
Nepomuk Crhonek <105591323+Nepomuk5665@users.noreply.github.com>
2025-05-14 13:02:43 +0200

    created Character-List.tsx and implemented it into App.tsx. Its not implemented correctly yet but i will add router later

--- rickandmorty/src/App.tsx
+++ rickandmorty/src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Authentication from './Authentication'
+import Characterlist from './Character-list'
 function App() {
   
 
   return (
     <>
       <Authentication />
-      
+      <Characterlist />
       
     </>
   )
---
+++ rickandmorty/src/Character-list.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Characterlist() {
+  return (
+    <div>
+      <h1>Character List</h1>
+    </div>
+  )
+}
