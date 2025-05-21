7b3349e758fd0f324e18569d4213f3d20e677375
Nepomuk Crhonek <105591323+Nepomuk5665@users.noreply.github.com>
2025-05-21 13:39:39 +0200

    dont check pwd lenght on signin

--- rickandmorty/src/Authentication.tsx
+++ rickandmorty/src/Authentication.tsx
@@ -25,18 +25,20 @@ export default function Authentication() {
     
     const validate = (values : string) => {
         const errors = {};
-      
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-          errors.email = 'Invalid email address';
+        
+            if (!values.email) {
+             errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+            }
+        if (isSignUp) {
+            if (!values.password) {
+            errors.password = 'Required';
+            } else if (values.password.length < 3) {
+            errors.password = 'Password must be at least 3 characters';
+            }
         }
-      
-        if (!values.password) {
-          errors.password = 'Required';
-        } else if (values.password.length < 3) {
-          errors.password = 'Password must be at least 3 characters';
-        }
+        
       
         return errors;
       };
